523d3534ce512f85c0f44151ea2a3bc909216598
jtsang02 <jtsang02@student.ubc.ca>
2021-08-08 18:19:39 -0700

    js

--- app.js
+++ app.js
@@ -3,64 +3,104 @@ const nav = document.querySelector("nav");
 const mobileBtnExit = document.getElementById("mobile-exit");
 
 class Compartment {
-  constructor (h, w, LD, actOpns,
-               sprk, group) {
-                this.h = h;
-                this.w = w;
-                //this.a = a;
-                this.LD = LD;
-                this.actOpns = actOpns;
-                //this.uOpns = uOpns;
-                //this.r = r;
-                this.sprk = sprk;
-                this.group = group;
-                //this.haz = haz;
-                //this.frr = frr;
-                //this.constr = constr;
-                //this.clad = clad;
-              }
-    
-  hazard () {
-    if(this.group === "E" || this.group === "F-1" || this.group === "F-2")
+  constructor(h, w, LD, actOpns, sprk, group) {
+    this.h = h;
+    this.w = w;
+    this.LD = LD;
+    this.actOpns = actOpns;
+    this.sprk = sprk;
+    this.group = group;
+  }
+
+  hazard() {
+    if (this.group === "E" || this.group === "F-1" || this.group === "F-2")
       return true;
-    else
-      return false;
   }
-  
-  area () {
+
+  area() {
     let h = this.h;
     let w = this.w;
-    return h*w;
+    return h * w;
   }
 
-  ratio () {
+  ratio() {
     let h = this.h;
     let w = this.w;
 
-		if (h/w > w/h)
-			return h/w;
-		else
-			return w/h;
+    if (h / w > w / h) return h / w;
+    else return w / h;
   }
 
-  ratioCode(){
-    let r = this.ratio();
-    if (r < 3)
-      return 1;
-    else if (r <=10)
-      return 2;
-    else
-      return 3;
+  ratioCode() {
+    let r = this.ratio;
+    if (r < 3) return 1;
+    else if (r <= 10) return 2;
+    else return 3;
+  }
+
+  getAO() {
+    return (this.actOpns / this.area()) * 100;
+  }
+
+  getUO() {
+    
+    /*var rowSize = 0,
+      columnSize = 0,
+      maxLD = 0,
+      maxArea = 0;
+    var x = 0,
+      y = 0,
+      shift_y = 0;
+    z = 0;
+    var LD1 = 0,
+      LD2 = 0,
+      area1 = 0,
+      area2 = 0;
+    var u1 = 0,
+      u2 = 0,
+      u3 = 0,
+      u4 = 0,
+      u = 0;*/
+
+    if (this.sprk) {
+
+      let shift_y = 1;
+
+      if (this.hazard) {        // table 3.2.3.1.E
+        let rowSize = 15;
+        let columnSize = 20;
+        let table = "tableE.txt";
+        let maxLD = 15;
+        let maxArea = 200;
+      }
+
+      if (!this.hazard) {        // table 3.2.3.1.D
+        let rowSize = 14;
+        let columnSize = 14;
+        let table = "tableD.txt";
+        let maxLD = 9;
+        let maxArea = 150;
+      }
+    }
+
+    if (!this.sprk) {
+      let shift_y = 3;
+      let z = this.ratioCode();
+      let rowSize = 55;
+      let maxArea = 2000;
+
+      if (this.hazard) {
+        let columnSize = 33;
+        let table = "tableC.txt";
+        maxLD = 70;
+      }
+    }
+
   }
 }
 
 let comp1 = new Compartment(2, 4, 5, 10, true, "D");
 let comp2 = new Compartment(3, 12, 5, 10, true, "E");
-
-console.log = "hazard is " + comp1.hazard();
-console.log = "area is " + comp1.area();
-console.log = "ratio is " + comp1.ratio();
-
 
 mobileBtn.addEventListener("click", () => {
   nav.classList.add("menu-btn");
